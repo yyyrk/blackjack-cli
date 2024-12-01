9ee8078b5b46689e192a84ee6f243c0abc2da538
Andrew Yurik <andrei.iurik@gmail.com>
2024-12-01 23:46:50 +0300

    start

---
+++ main_old.rb
@@ -0,0 +1,15 @@
+require 'ruby2d'
+
+set title: "Blackjack Game"
+set width: 640, height: 480
+
+Image.new('./graphics/background_small.png')
+
+boom = Sound.new('./audio/8_Bit_music.mp3')
+
+# Play the sound
+boom.play
+
+
+
+show
