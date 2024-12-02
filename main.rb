ab18adb4fb2868acc4ef72437988dd38f69836c4
Andrew Yurik <andrei.iurik@gmail.com>
2024-12-02 15:53:49 +0300

    rough draft № 2

--- main.rb
+++ main.rb
@@ -3,45 +3,48 @@ class Player
     @cards = []
     @balance = 100
   end
+
+  def add_card(card)
+    @cards << card
+  end
+  def reset_cards;  end # cбрасывает карты перед новым раундом
+
   def points
     @balance
   end
-  def make_move
+  def make_move;  end
 
-  end
+
 end
 
-class User < Player
+class User < Player  # у него есть эти опции:take :skip :open
   def make_move
     puts 1.# деламе ходы и на выбор действия, потом проверяю в отдельном методе приватном
 
-      case 1 #:take :skip :open
+      case 1
     :take
   end
 end
 
-class Dealer < Player #можно сделать дилера чтобы он на угад ходил
+class Dealer < Player #можно сделать дилера чтобы он на угад ходил в будущем
   def make_move
     points > 17 ? :take : :skip
   end
 end
 
-class Deck
+class Deck #здесь управляем колодой карт.
   initialize
   # @cards = 52.times.map do @cards << Card.new #Псевдокод, где 52 раза генерим карты
   def take_card
     generate_deck if @cards.empty?
   end
 
-
   private
-  def shuffle_cards
-  end
-
-
+  def shuffle_cards; end
+  def generate_deck; end
 end
 
-class Round
+class Round #этот класс это абстракция игрового раунда
   def initialize(players, deck)
     @players = players
     @deck = deck
@@ -60,7 +63,7 @@ class Round
     player.cards << deck.take_card #по 2 раза
     end
 
-    def make_moves; end
+    def make_moves; end # это цикл ходов игроков
 
     def define_winner
       @players.sample
@@ -84,7 +87,7 @@ class Game
 
   def start
     loop do
-    take_bets#сделать ставки/ Ее смысл что берет у каждого игрока баланс и уменьшает на 10, если баланс 0, то игра окончена, то высести результаты
+    take_bets # сделать ставки/ Ее смысл что берет у каждого игрока баланс и уменьшает на 10, если баланс 0, то игра окончена, то высести результаты
     play_round
     calculate_results # берет игроков, смотрит их очки и начисляет выйгрыш
     end
@@ -93,13 +96,17 @@ class Game
   end
 
   private
-  def take_bets
+  def take_bets #обрабатываем здесь ставки игроков
 
   end
   def play_round
     Round.new(@players, @deck).play
   end
-  def calculate_results
+  def calculate_results # обновляет результаты (балансы игроков)
+
+  end
+
+  def process_winner(winner) # выводит информацию о победителе
 
   end
 end
